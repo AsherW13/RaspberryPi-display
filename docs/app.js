9b918597990fb4e5b1ee89a50fd8d2a3bd5dcba8
AsherW13 <aweitz@zagmail.gonzaga.edu>
2025-05-05 19:23:43 -0700

    removed commented code, was getting found

--- docs/app.js
+++ docs/app.js
@@ -1,26 +1,3 @@
-// const matrixContainer = document.getElementById('matrix');
-
-// // const socket = io('http://');
-
-// function createMatrix() {
-//   for (let i = 0; i < 64; i++) {
-//     const div = document.createElement('div');
-//     div.classList.add('pixel');
-//     matrixContainer.appendChild(div);
-//   }
-// }
-
-// socket.on('matrix_data', (pixels) => {
-//   const pixelDivs = document.querySelectorAll('.pixel');
-//   pixels.forEach((rgb, i) => {
-//     const color = `rgb(${rgb[0]},${rgb[1]},${rgb[2]})`;
-//     pixelDivs[i].style.backgroundColor = color;
-//   });
-// });
-
-// socket.emit('get_matrix');
-// createMatrix();
-
 const grid = document.getElementById("grid");
 
 // Create 8x8 grid
