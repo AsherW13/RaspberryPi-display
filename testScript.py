e5736a675de45889f35b81f683617fec79eaae1e
AsherW13 <aweitz@zagmail.gonzaga.edu>
2025-05-17 18:03:00 -0700

    Attempting to get double click for removing set pixel

--- testScript.py
+++ testScript.py
@@ -6,18 +6,17 @@ sense = SenseHat()
 sense.clear()
 
 
-set_pixels = set()
+set_pixels = {}
 pixel_state = [[False for _ in range(8)] for _ in range (8)]
 
 x = 0
 y = 0
 move_color = (255, 255, 255)
 i = 0
-
+press_time = 0
 def set_pixel():
-	for lx, ly in set_pixels:
-		temp_color = (random.randint(0, 255), random.randint(0, 255), random.randint(0, 255))
-		sense.set_pixel(lx, ly, temp_color)
+	for (lx, ly), color in set_pixels.items():
+		sense.set_pixel(lx, ly, color)
 
 def update_display(x, y, color):
 	sense.clear()
@@ -25,7 +24,7 @@ def update_display(x, y, color):
 	sense.set_pixel(x, y, color)
 	
 def move(event):
-	global x, y, color
+	global x, y, color, press_time
 	if event.action == 'pressed':
 		if event.direction == 'up' and y > 0:
 			y -= 1
@@ -36,9 +35,17 @@ def move(event):
 		elif event.direction == 'right' and x < 7:
 			x += 1
 		elif event.direction == 'middle':
-			set_pixels.add((x, y))
-			set_pixel()
-			return
+			now = time.time()
+			if now - press_time < 0.3:
+				if(x, y) in set_pixels:
+					del set_pixels[(x,y)]
+					update_display(x, y, move_color)
+			else:
+				temp_color = (random.randint(0, 255), random.randint(0, 255), random.randint(0, 255))
+				set_pixels[(x,y)] = temp_color 
+				set_pixel()
+				return
+			press_time = now
 		else:
 			print(f"cannot go out of bounds\n")
 		update_display(x,y, move_color)
@@ -58,6 +65,9 @@ try:
 		time.sleep(0.1)
 except KeyboardInterrupt:
 	sense.clear()
+	
+	
+	
 #try:
 #	while(i < 20):
 #		x = random.randint(0, 7)
