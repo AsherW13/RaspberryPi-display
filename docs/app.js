eb9dbc108419836b3fee2ef8a3b1546a94dcb423
AsherW13 <aweitz@zagmail.gonzaga.edu>
2025-05-18 18:42:08 -0700

    Creating rgb color picker option for web app interface

--- docs/app.js
+++ docs/app.js
@@ -12,3 +12,33 @@ socket.on("pixel_update", ({ x, y, color }) => {
   const pixel = document.querySelectorAll(".pixel")[index];
   pixel.style.backgroundColor = `rgb(${color[0]}, ${color[1]}, ${color[2]})`;
 });
+
+const redSlider = document.getElementById('red');
+const greenSlider = document.getElementById('green');
+const blueSlider = document.getElementById('blue');
+
+const colorBox = document.getElementById('colorBox');
+
+const redValue = document.getElementById('redValue');
+const greenValue = document.getElementById('greenValue');
+const blueValue = document.getElementById('blueValue');
+
+
+function updateColor() {
+  const red = redSlider.value;
+  const green = greenSlider.value;
+  const blue = blueSlider.value;
+
+  colorBox.style.backgroundColor = `rgb(${red}, ${green}, ${blue})`;
+
+  redValue.textContent = red;
+  greenValue.textContent = green;
+  blueValue.textContent = blue;
+
+}
+
+redSlider.addEventListener('input', updateColor);
+greenSlider.addEventListener('input', updateColor);
+blueSlider.addEventListener('input', updateColor);
+
+updateColor();
