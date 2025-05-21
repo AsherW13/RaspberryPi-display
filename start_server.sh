6bdc62ddd5be7d23e2296c665d39d7297f2c4c55
AsherW13 <aweitz@zagmail.gonzaga.edu>
2025-05-21 15:23:25 -0700

    server script

---
+++ start_server.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+cd /home/aweitz/Desktop/DisplayProj/RaspberryPi-display
+
+ngrok http http://localhost:5000 > /dev/null &
+
+sleep 5
+
+#retrieves random public url
+NGROK_URL=$(curl -s http://localhost:4040/api/tunnels | jq -r '.tunnels[0].public_url')
+
+echo "{\"url\": \"$NGROK_URL\"}" > /home/aweitz/Desktop/DisplayProj/RaspberryPi-display/ngrok_url.json
+
+echo "Ngrok public url: $NGROK_URL"
+
+source venv/bin/activate
+# starts Flask app
+python3 app/server.py &
+
+sleep 5
